80efc98c2a4971e7b9fc902dd6eb06b7637360b7
Nayan <nayana@mindfiresolutions.com>
2019-02-06 12:56:51 +0530

    Changed Method type from private to public

--- springcustomannotations/src/main/java/com/java/timelogger/controller/ProjectController.java
+++ springcustomannotations/src/main/java/com/java/timelogger/controller/ProjectController.java
@@ -2,7 +2,6 @@ package com.java.timelogger.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.java.timelogger.log.LogExecutionTime;
@@ -16,7 +15,8 @@ public class ProjectController {
 	ProjectService projectService;
 
 	@GetMapping("/testTimeTaken")
-	private String testTimeTaken() {
+	@LogExecutionTime
+	public String testTimeTaken() {
 		return projectService.dummyMethod();
 	}
 
